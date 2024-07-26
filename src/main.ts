4e62409056ec035776311a73575060b2d8e1eb73
KennerEspinal <kennerespinalg@gmail.com>
2024-07-25 22:57:03 -0500

    Add ConfigModule

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const logger = new Logger('App');
 
   const config = new DocumentBuilder()
     .setTitle('RESTFull API with NestJS')
@@ -14,5 +17,8 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(3000);
+
+  logger.log(`Server is running on: ${process.env.HOST}:${process.env.PORT}`);
+  logger.log(`Documentation: ${process.env.HOST}:${process.env.PORT}/api/docs`);
 }
 bootstrap();
